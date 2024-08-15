a3760ac2ca7d6bc1739c84d6657d80221a04513d
eric fleming <efleming111@yahoo.com>
2024-08-15 07:27:05 -0700

    Finished LearningGodot, started ProjectBoost

---
+++ ProjectBoost/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class Player : Node3D
+{
+
+
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionPressed("ui_accept"))
+		{
+			Position = new Vector3(0.0f, Position.Y + (float)delta, 0.0f);
+		}
+
+        if (Input.IsActionPressed("ui_left"))
+        {
+            Rotation = new Vector3(0.0f, 0.0f, Rotation.Z + (float)delta);
+        }
+
+        if (Input.IsActionPressed("ui_right"))
+        {
+            Rotation = new Vector3(0.0f, 0.0f, Rotation.Z - (float)delta);
+        }
+    }
+}
