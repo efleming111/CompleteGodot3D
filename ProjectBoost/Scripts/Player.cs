9ca89029d46032a56ce470c28cd6e08f86c91920
efleming111 <efleming111@yahoo.com>
2024-08-15 19:22:13 -0700

    Added physics based player movement to ProjectBoost

--- ProjectBoost/Scripts/Player.cs
+++ ProjectBoost/Scripts/Player.cs
@@ -2,29 +2,24 @@ using Godot;
 using System;
 using System.Diagnostics;
 
-public partial class Player : Node3D
+public partial class Player : RigidBody3D
 {
 
-    public override void _Ready()
-    {
-        Debug.Print("Ready Player");
-    }
-
-    public override void _Process(double delta)
+	public override void _Process(double delta)
 	{
 		if (Input.IsActionPressed("ui_accept"))
 		{
-			Position = new Vector3(0.0f, Position.Y + (float)delta, 0.0f);
+			ApplyCentralForce((float)delta * 1000.0f * Basis.Y);
 		}
 
-        if (Input.IsActionPressed("ui_left"))
-        {
-            Rotation = new Vector3(0.0f, 0.0f, Rotation.Z + (float)delta);
-        }
+		if (Input.IsActionPressed("ui_left"))
+		{
+			ApplyTorque(new Vector3(0.0f, 0.0f, 100.0f * (float)delta));
+		}
 
-        if (Input.IsActionPressed("ui_right"))
-        {
-            Rotation = new Vector3(0.0f, 0.0f, Rotation.Z - (float)delta);
+		if (Input.IsActionPressed("ui_right"))
+		{
+            ApplyTorque(new Vector3(0.0f, 0.0f, -100.0f * (float)delta));
         }
-    }
+	}
 }
